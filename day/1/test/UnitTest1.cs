e07ffac13f9c0586c75a2fbd5627eaa1291bc8f0
Mike Branstein <mike.branstein@gmail.com>
2019-12-01 15:55:50 -0500

    finish challenge 1

--- day/1/test/UnitTest1.cs
+++ day/1/test/UnitTest1.cs
@@ -1,14 +1,77 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace test
 {
-    [TestClass]
-    public class UnitTest1
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void CanRun()
     {
-        [TestMethod]
-        public void CanRun()
-        {
-          
-        }
+      // arrange
+      var program = new challenge.Program();
+
+      // act
+      program.ExecuteProgram(String.Empty);
+
+      // assert
     }
+
+    [TestMethod]
+    public void Mass_12_returns_2()
+    {
+      // arrange
+      var program = new challenge.Program();
+      var mass = 12;
+
+      // act
+      var fuel = program.CalculateFuel(mass);
+
+      // assert
+      Assert.AreEqual(fuel, 2);
+    }
+
+    [TestMethod]
+    public void Mass_14_returns_2()
+    {
+      // arrange
+      var program = new challenge.Program();
+      var mass = 14;
+
+      // act
+      var fuel = program.CalculateFuel(mass);
+
+      // assert
+      Assert.AreEqual(fuel, 2);
+    }
+
+    [TestMethod]
+    public void Mass_1969_returns_654()
+    {
+      // arrange
+      var program = new challenge.Program();
+      var mass = 1969;
+
+      // act
+      var fuel = program.CalculateFuel(mass);
+
+      // assert
+      Assert.AreEqual(fuel, 654);
+    }
+
+    [TestMethod]
+    public void Mass_100756_returns_33583()
+    {
+      // arrange
+      var program = new challenge.Program();
+      var mass = 100756;
+
+      // act
+      var fuel = program.CalculateFuel(mass);
+
+      // assert
+      Assert.AreEqual(fuel, 33583);
+    }
+  }
 }
