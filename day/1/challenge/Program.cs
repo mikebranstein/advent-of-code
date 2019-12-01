a745c6e85c4fa929b738c3ba92e5e58fb5e8e0c1
Mike Branstein <mike.branstein@gmail.com>
2019-12-01 10:28:21 -0500

    initial commit

---
+++ day/1/challenge/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+
+            //var totalFuel = program.ExecuteProgram("input.txt");
+            //Console.WriteLine($"Fuel required is: {totalFuel}.");
+        }
+
+        public int ExecuteProgram(string input) {
+            var totalFuel = 0;
+
+            var fileStream = new FileStream(input, FileMode.Open);
+            using (var streamReader = new StreamReader(fileStream)) {
+                while (!streamReader.EndOfStream) {
+                    var line = streamReader.ReadLine();
+                    var mass = int.Parse(line);
+
+                    totalFuel += CalculateFuel(mass);
+                }
+            }
+
+            return totalFuel;
+        }
+
+        private int CalculateFuel(int mass) {
+            return ((int) Math.Floor(mass / 3.0)) - 2;
+        }
+    }
+}
