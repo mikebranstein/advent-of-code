7c4c1aa68fa1a543b94e79ea86bbc607db0f5a86
Mike Branstein <mike.branstein@gmail.com>
2019-12-02 11:58:54 -0500

    day 1 challenge 1 completed

---
+++ day/2/challenge/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace challenge
+{
+  public class Program
+  {
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Running program.");
+      Console.WriteLine("");
+
+      var program = new Program();
+      var result = program.Run("input.txt");
+
+      Console.WriteLine($"End state: {result}");
+      Console.WriteLine("");
+
+
+      Console.WriteLine("Finished running program.");
+      Console.ReadLine();
+    }
+
+    public string Run(string inputFileName)
+    {
+      // get code
+      var code = IngestCode(inputFileName);
+
+      Console.WriteLine($"Initial state: {ConvertCodeToString(code)}");
+      Console.WriteLine("");
+
+      // Once you have a working computer, the first step is to restore the gravity
+      // assist program(your puzzle input) to the "1202 program alarm" state it had
+      // just before the last computer caught fire. To do this, before running the
+      // program, replace position 1 with the value 12 and replace position 2 with
+      // the value 2.What value is left at position 0 after the program halts?
+
+      code[1] = 12;
+      code[2] = 2;
+
+      Console.WriteLine($"1202 Program Alert state: {ConvertCodeToString(code)}");
+      Console.WriteLine("");
+
+
+      var resultingCode = ExecuteSteps(code);
+      var codeString = ConvertCodeToString(resultingCode);
+
+      return codeString;
+    }
+
+
+    public List<int> IngestCode(string inputFileName)
+    {
+      var code = new List<int>();
+
+      var fileStream = new FileStream(inputFileName, FileMode.Open);
+      using (var streamReader = new StreamReader(fileStream))
+      {
+        if (!streamReader.EndOfStream)
+        {
+          var codeLine = streamReader.ReadLine();
+
+          var codeArray = codeLine.Split(",");
+          foreach (var codeFragment in codeArray)
+          {
+            // convert to integer and add to list
+            code.Add(int.Parse(codeFragment));
+          }
+        }
+      }
+
+      return code;
+    }
+
+    // outputs number of steps it's read
+    public List<int> ExecuteSteps(List<int> inputCode)
+    {
+      var code = inputCode;
+      int stepNumber = 0;
+
+      for(var index = 0; index <= inputCode.Count; index += 4)
+      {
+        var operation = ParseOperation(inputCode, index);
+        if (operation.Type == 1) code = ProcessOperation1(code, operation);
+        else if (operation.Type == 2) code = ProcessOperation2(code, operation);
+        else if (operation.Type == 99) break; // 99 menas end of program
+
+        stepNumber++;
+      }
+
+      return code;
+    }
+
+    public Operation ParseOperation(List<int> code, int stepIndex)
+    {
+      var operation = new Operation() { Type = code[stepIndex] };
+
+      // operation 99 is end of program
+      if (operation.Type == 99) return operation;
+
+      // other operations have more data
+      operation.ReadAddress1 = code[stepIndex + 1];
+      operation.ReadAddress2 = code[stepIndex + 2];
+      operation.OutpotPosition = code[stepIndex + 3];
+
+      return operation;
+    }
+
+    public List<int> ProcessOperation1(List<int> code, Operation operation)
+    {
+      var valueOfAddress1 = code[operation.ReadAddress1.Value];
+      var valueOfAddress2 = code[operation.ReadAddress2.Value];
+
+      // addition
+      var result = valueOfAddress1 + valueOfAddress2;
+
+      // update code
+      code[operation.OutpotPosition.Value] = result;
+
+      return code;
+    }
+
+    public List<int> ProcessOperation2(List<int> code, Operation operation)
+    {
+      var valueOfAddress1 = code[operation.ReadAddress1.Value];
+      var valueOfAddress2 = code[operation.ReadAddress2.Value];
+
+      // addition
+      var result = valueOfAddress1 * valueOfAddress2;
+
+      // update code
+      code[operation.OutpotPosition.Value] = result;
+
+      return code;
+    }
+
+    public string ConvertCodeToString(List<int> code)
+    {
+      var codeString = "";
+
+      foreach(var item in code)
+      {
+        codeString += item.ToString();
+        codeString += ",";
+      }
+
+      // strip last character off end - ,
+      return codeString.Remove(codeString.Length - 1);
+    }
+  }
+
+  public class Operation
+  {
+    public int Type { get; set; }
+    public int? ReadAddress1 { get; set; }
+    public int? ReadAddress2 { get; set; }
+    public int? OutpotPosition { get; set; }
+  }
+}
---
+++ day/2/test/UnitTest1.cs
@@ -0,0 +1,338 @@
+using System.Collections.Generic;
+using challenge;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace test
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void Can_Ingest_Code_1()
+    {
+      // arrange
+      var program = new Program();
+
+      // act
+      var code = program.IngestCode("test_input_1.txt");
+
+      // assert
+      Assert.AreEqual(code.Count, 5);
+      Assert.AreEqual(code[0], 1);
+      Assert.AreEqual(code[1], 1);
+      Assert.AreEqual(code[2], 2);
+      Assert.AreEqual(code[3], 3);
+      Assert.AreEqual(code[4], 99);
+    }
+
+
+    [TestMethod]
+    public void Can_Identify_Operation_1()
+    {
+      // arrange
+      var program = new Program();
+      var code = program.IngestCode("op_1.txt");
+
+      // act
+      var operation = program.ParseOperation(code, 0);
+
+      // assert no exception thrown
+      Assert.AreEqual(operation.Type, 1);
+      Assert.AreEqual(operation.ReadAddress1, 2);
+      Assert.AreEqual(operation.ReadAddress2, 3);
+      Assert.AreEqual(operation.OutpotPosition, 4);
+    }
+
+
+    [TestMethod]
+    public void Can_Identify_Operation_2()
+    {
+      // arrange
+      var program = new Program();
+      var code = program.IngestCode("op_2.txt");
+
+      // act
+      var operation = program.ParseOperation(code, 0);
+
+      // assert no exception thrown
+      Assert.AreEqual(operation.Type, 2);
+      Assert.AreEqual(operation.ReadAddress1, 1);
+      Assert.AreEqual(operation.ReadAddress2, 3);
+      Assert.AreEqual(operation.OutpotPosition, 4);
+    }
+
+
+    [TestMethod]
+    public void Can_Identify_Operation_99()
+    {
+      // arrange
+      var program = new Program();
+      var code = program.IngestCode("op_99.txt");
+
+      // act
+      var operation = program.ParseOperation(code, 4);
+
+      // assert 
+      Assert.AreEqual(operation.Type, 99);
+      Assert.IsFalse(operation.ReadAddress1.HasValue);
+      Assert.IsFalse(operation.ReadAddress2.HasValue);
+      Assert.IsFalse(operation.OutpotPosition.HasValue);
+    }
+
+    [TestMethod]
+    public void Can_Process_Operation_1_Add_Simple()
+    {
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 1, 0, 3, 3, 99 };
+
+      // act
+      var operation = program.ParseOperation(code, 0);
+      var resultingCode = program.ProcessOperation1(code, operation);
+
+      // assert code not changed
+      Assert.AreEqual(resultingCode[0], 1);
+      Assert.AreEqual(resultingCode[1], 0);
+      Assert.AreEqual(resultingCode[2], 3);
+      Assert.AreEqual(resultingCode[3], 4);
+      Assert.AreEqual(resultingCode[4], 99);
+    }
+
+    [TestMethod]
+    public void Can_Process_Operation_1_Add_Chain()
+    {
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 1, 3, 2, 5, 1, 0, 0, 1, 99 };
+
+      // act - add one
+      var operation = program.ParseOperation(code, 0);
+      var resultingCode = program.ProcessOperation1(code, operation);
+
+      // assert 
+      Assert.AreEqual(resultingCode[0], 1);
+      Assert.AreEqual(resultingCode[1], 3);
+      Assert.AreEqual(resultingCode[2], 2);
+      Assert.AreEqual(resultingCode[3], 5);
+      Assert.AreEqual(resultingCode[4], 1);
+      Assert.AreEqual(resultingCode[5], 7);
+      Assert.AreEqual(resultingCode[6], 0);
+      Assert.AreEqual(resultingCode[7], 1);
+      Assert.AreEqual(resultingCode[8], 99);
+
+      // act - second add
+      var operation2 = program.ParseOperation(resultingCode, 4);
+      var resultingCode2 = program.ProcessOperation1(resultingCode, operation2);
+
+      // assert second operation
+      Assert.AreEqual(resultingCode2[0], 1);
+      Assert.AreEqual(resultingCode2[1], 2);
+      Assert.AreEqual(resultingCode2[2], 2);
+      Assert.AreEqual(resultingCode2[3], 5);
+      Assert.AreEqual(resultingCode2[4], 1);
+      Assert.AreEqual(resultingCode2[5], 7);
+      Assert.AreEqual(resultingCode2[6], 0);
+      Assert.AreEqual(resultingCode2[7], 1);
+      Assert.AreEqual(resultingCode2[8], 99);
+    }
+
+    [TestMethod]
+    public void Can_Process_Operation_2_Multiply_Simple()
+    {
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 2, 0, 3, 5, 99, 0 };
+
+      // act
+      var operation = program.ParseOperation(code, 0);
+      var resultingCode = program.ProcessOperation2(code, operation);
+
+      // assert code not changed
+      Assert.AreEqual(resultingCode[0], 2);
+      Assert.AreEqual(resultingCode[1], 0);
+      Assert.AreEqual(resultingCode[2], 3);
+      Assert.AreEqual(resultingCode[3], 5);
+      Assert.AreEqual(resultingCode[4], 99);
+      Assert.AreEqual(resultingCode[5], 10);
+    }
+
+    [TestMethod]
+    public void Can_Process_Operation_2_Multiply_Chain()
+    {
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 2, 3, 2, 5, 2, 0, 0, 1, 99, 20, 30};
+
+      // act - add one
+      var operation = program.ParseOperation(code, 0);
+      var resultingCode = program.ProcessOperation2(code, operation);
+
+      // assert 
+      Assert.AreEqual(resultingCode[0], 2);
+      Assert.AreEqual(resultingCode[1], 3);
+      Assert.AreEqual(resultingCode[2], 2);
+      Assert.AreEqual(resultingCode[3], 5);
+      Assert.AreEqual(resultingCode[4], 2);
+      Assert.AreEqual(resultingCode[5], 10);
+      Assert.AreEqual(resultingCode[6], 0);
+      Assert.AreEqual(resultingCode[7], 1);
+      Assert.AreEqual(resultingCode[8], 99);
+      Assert.AreEqual(resultingCode[9], 20);
+      Assert.AreEqual(resultingCode[10], 30);
+
+      // act - second add
+      var operation2 = program.ParseOperation(resultingCode, 4);
+      var resultingCode2 = program.ProcessOperation2(resultingCode, operation2);
+
+      // assert second operation
+      Assert.AreEqual(resultingCode2[0], 2);
+      Assert.AreEqual(resultingCode2[1], 60);
+      Assert.AreEqual(resultingCode2[2], 2);
+      Assert.AreEqual(resultingCode2[3], 5);
+      Assert.AreEqual(resultingCode2[4], 2);
+      Assert.AreEqual(resultingCode2[5], 10);
+      Assert.AreEqual(resultingCode2[6], 0);
+      Assert.AreEqual(resultingCode2[7], 1);
+      Assert.AreEqual(resultingCode2[8], 99);
+      Assert.AreEqual(resultingCode2[9], 20);
+      Assert.AreEqual(resultingCode2[10], 30);
+    }
+
+    [TestMethod]
+    public void Can_Process_Multi_Steps_1()
+    {
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 2, 3, 2, 5, 2, 0, 0, 1, 99, 20, 30 };
+
+      // act
+      var resultingCode = program.ExecuteSteps(code);
+
+      // assert second operation
+      Assert.AreEqual(resultingCode[0], 2);
+      Assert.AreEqual(resultingCode[1], 60);
+      Assert.AreEqual(resultingCode[2], 2);
+      Assert.AreEqual(resultingCode[3], 5);
+      Assert.AreEqual(resultingCode[4], 2);
+      Assert.AreEqual(resultingCode[5], 10);
+      Assert.AreEqual(resultingCode[6], 0);
+      Assert.AreEqual(resultingCode[7], 1);
+      Assert.AreEqual(resultingCode[8], 99);
+      Assert.AreEqual(resultingCode[9], 20);
+      Assert.AreEqual(resultingCode[10], 30);
+    }
+
+    [TestMethod]
+    public void Can_Process_Multi_Steps_2()
+    {
+      // 1,9,10,3,2,3,11,0,99,30,40,50
+
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 };
+
+      // act
+      var resultingCode = program.ExecuteSteps(code);
+
+      // assert second operation
+      Assert.AreEqual(resultingCode[0], 3500);
+      Assert.AreEqual(resultingCode[1], 9);
+      Assert.AreEqual(resultingCode[2], 10);
+      Assert.AreEqual(resultingCode[3], 70);
+      Assert.AreEqual(resultingCode[4], 2);
+      Assert.AreEqual(resultingCode[5], 3);
+      Assert.AreEqual(resultingCode[6], 11);
+      Assert.AreEqual(resultingCode[7], 0);
+      Assert.AreEqual(resultingCode[8], 99);
+      Assert.AreEqual(resultingCode[9], 30);
+      Assert.AreEqual(resultingCode[10], 40);
+      Assert.AreEqual(resultingCode[11], 50);
+    }
+
+    [TestMethod]
+    public void Small_Program_1()
+    {
+      // 1,0,0,0,99
+
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 1, 0, 0, 0, 99 };
+
+      // act
+      var resultingCode = program.ExecuteSteps(code);
+
+      // assert second operation
+      // 2,0,0,0,99
+      Assert.AreEqual(resultingCode[0], 2);
+      Assert.AreEqual(resultingCode[1], 0);
+      Assert.AreEqual(resultingCode[2], 0);
+      Assert.AreEqual(resultingCode[3], 0);
+      Assert.AreEqual(resultingCode[4], 99);
+    }
+
+    [TestMethod]
+    public void Small_Program_2()
+    {
+      // 2,3,0,3,99
+
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 2, 3, 0, 3, 99 };
+
+      // act
+      var resultingCode = program.ExecuteSteps(code);
+
+      // assert second operation
+      // 2,3,0,6,99
+      Assert.AreEqual(resultingCode[0], 2);
+      Assert.AreEqual(resultingCode[1], 3);
+      Assert.AreEqual(resultingCode[2], 0);
+      Assert.AreEqual(resultingCode[3], 6);
+      Assert.AreEqual(resultingCode[4], 99);
+    }
+
+    [TestMethod]
+    public void Small_Program_3()
+    {
+      // arrange
+      var program = new Program();
+
+      // act
+      var codeString = program.Run("small_program_3.txt");
+
+      // assert second operation
+      // 2,4,4,5,99,9801
+      Assert.AreEqual(codeString, "2,4,4,5,99,9801");
+    }
+
+    [TestMethod]
+    public void Can_Convert_Code_to_String()
+    {
+      // arrange
+      var program = new Program();
+      var code = new List<int> { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 };
+
+      // act
+      var codeString = program.ConvertCodeToString(code);
+
+      // assert 
+      Assert.AreEqual(codeString, "1,9,10,3,2,3,11,0,99,30,40,50");
+    }
+
+    [TestMethod]
+    public void Small_Program_4()
+    {
+      // arrange
+      var program = new Program();
+
+      // act
+      var codeString = program.Run("small_program_4.txt");
+
+      // assert second operation
+      // 30,1,1,4,2,5,6,0,99
+      Assert.AreEqual(codeString, "30,1,1,4,2,5,6,0,99");
+    }
+  }
+
+
+}
