833dbde0735aabef33b4be4ea43e48308f096eb3
Mike Branstein <mike.branstein@gmail.com>
2019-12-05 22:29:46 -0500

    add long opcode detection

--- computer/IntcodeComputer/InstructionFactory.cs
+++ computer/IntcodeComputer/InstructionFactory.cs
@@ -8,32 +8,34 @@ namespace IntcodeComputer
     public static IInstruction ParseInstruction(List<int> memory, int instructionPointer)
     {
       var opCode = memory[instructionPointer];
+      var opCodeString = opCode.ToString().PadLeft(2); // 2-digit op codes
+      var baseOpCode = int.Parse(opCodeString.Replace(' ', '0').Substring(opCodeString.Length - 2));
 
-      if (opCode == 1)
+      if (baseOpCode == 1)
         return new AddInstruction(
           opCode,
           memory[instructionPointer + 1],
           memory[instructionPointer + 2],
           memory[instructionPointer + 3]);
 
-      else if (opCode == 2)
+      else if (baseOpCode == 2)
         return new MultiplyInstruction(
           opCode,
           memory[instructionPointer + 1],
           memory[instructionPointer + 2],
           memory[instructionPointer + 3]);
 
-      else if (opCode == 3)
+      else if (baseOpCode == 3)
         return new InputInstruction(
           opCode,
           memory[instructionPointer + 1]);
 
-      else if (opCode == 4)
+      else if (baseOpCode == 4)
         return new OutputInstruction(
           opCode,
           memory[instructionPointer + 1]);
 
-      else if (opCode == 99) return new HaltInstruction();
+      else if (baseOpCode == 99) return new HaltInstruction();
 
       throw new Exception($"Invalid OpCode detected: {opCode}");
     }
---
+++ computer/IntcodeComputerTest/ParameterModeUnitTest.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections.Generic;
+using IntcodeComputer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IntcodeComputerTest
+{
+  [TestClass]
+  public class ParameterModeUnitTest
+  {
+
+    [TestMethod]
+    public void Can_Identify_Long_OpCodes_Add()
+    {
+      // arrange
+      var memory = new List<int>() { 01101, 0, 2, 3, 99 };
+
+      // act
+      var instruction = (AddInstruction)InstructionFactory.ParseInstruction(memory, 0);
+
+      // assert 
+      Assert.AreEqual(instruction.OpCode, 1);
+      Assert.AreEqual(instruction.Parameter1.Value, 0);
+      Assert.AreEqual(instruction.Parameter2.Value, 2);
+      Assert.AreEqual(instruction.Parameter3.Value, 3);
+      Assert.AreEqual(instruction.PointerAdvancement, 4);
+    }
+
+    [TestMethod]
+    public void Can_Identify_Long_OpCodes_Multiply()
+    {
+      // arrange
+      var memory = new List<int>() { 01102, 0, 2, 3, 99 };
+
+      // act
+      var instruction = (MultiplyInstruction)InstructionFactory.ParseInstruction(memory, 0);
+
+      // assert 
+      Assert.AreEqual(instruction.OpCode, 2);
+      Assert.AreEqual(instruction.Parameter1.Value, 0);
+      Assert.AreEqual(instruction.Parameter2.Value, 2);
+      Assert.AreEqual(instruction.Parameter3.Value, 3);
+      Assert.AreEqual(instruction.PointerAdvancement, 4);
+    }
+
+    [TestMethod]
+    public void Can_Identify_Long_OpCodes_Input()
+    {
+      // arrange
+      var memory = new List<int>() { 01103, 0, 99 };
+
+      // act
+      var instruction = (InputInstruction)InstructionFactory.ParseInstruction(memory, 0);
+
+      // assert 
+      Assert.AreEqual(instruction.OpCode, 3);
+      Assert.AreEqual(instruction.Parameter1.Value, 0);
+      Assert.AreEqual(instruction.PointerAdvancement, 2);
+    }
+
+    [TestMethod]
+    public void Can_Identify_Long_OpCodes_Output()
+    {
+      // arrange
+      var memory = new List<int>() { 01104, 0, 99 };
+
+      // act
+      var instruction = (OutputInstruction)InstructionFactory.ParseInstruction(memory, 0);
+
+      // assert 
+      Assert.AreEqual(instruction.OpCode, 4);
+      Assert.AreEqual(instruction.Parameter1.Value, 0);
+      Assert.AreEqual(instruction.PointerAdvancement, 2);
+    }
+
+
+    [TestMethod]
+    public void Can_Identify_Long_OpCodes_Halt()
+    {
+      // arrange
+      var memory = new List<int>() { 99 };
+
+      // act
+      var instruction = (HaltInstruction)InstructionFactory.ParseInstruction(memory, 0);
+
+      // assert 
+      Assert.AreEqual(instruction.OpCode, 99);
+      Assert.AreEqual(instruction.PointerAdvancement, 1);
+    }
+  }
+}
